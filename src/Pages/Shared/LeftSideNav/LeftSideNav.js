20f9a754ee2f9be0a952ea186f4386b3d76048f3
sheikh-momin <momin.emon14@gmail.com>
2022-10-25 00:42:49 -0700

    Basic setup

---
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState } from 'react';
+
+const LeftSideNav = () => {
+  const [categories, setCategories]=useState([])
+  return (
+    <div>
+      <h4>Course Category</h4>
+    </div>
+  );
+};
+
+export default LeftSideNav;
---
+++ src/layout/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from '../Pages/Shared/Header';
+import LeftSideNav from '../Pages/Shared/LeftSideNav/LeftSideNav';
+
+const Main = () => {
+  return (
+    <div>
+    <Header></Header>
+      <Container>
+        <Row>
+          <Col lg="2"><LeftSideNav></LeftSideNav></Col>
+          <Col lg="8">
+            <Outlet></Outlet>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Main;
