5d1c056f96a557d8dc160bf64c4b168b27a409da
sheikh-momin <momin.emon14@gmail.com>
2022-10-26 03:40:33 -0700

    sign out and handle form

--- src/SignIn/SignIn.js
+++ src/SignIn/SignIn.js
@@ -4,6 +4,7 @@ import { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { FaGithub, FaGoogle } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../Context/AuthProvider/AuthProvider';
 
 const SignIn = () => {
@@ -29,23 +30,38 @@ const SignIn = () => {
     .catch(error => console.error(error))
   }
 
+  const handleSubmit= event =>{
+    event.preventDefault()
+  }
+
   return (
     <div className='container'>
-      <Form>
+      <Form onSubmit={handleSubmit}> 
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Your Name</Form.Label>
+          <Form.Control name='name' type="text" placeholder="Enter email" />
+          
+        </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control name='email' type="email" placeholder="Enter email" required/>
           
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control name='password' type="password" placeholder="Password" required/>
         </Form.Group>
         
-        <Button variant="primary" type="submit">
-          Sign In
-        </Button>
+        <div className='d-flex '>
+          <Button className='me-2' variant="primary" type="submit">
+            Sign In
+          </Button>
+          <Link to='/login'><p className='mt-2'>You already have an account?Then Log In!</p></Link>
+        </div>
+        <Form.Text className='text-danger'>
+
+        </Form.Text>
         <div className='mt-3 d-flex justify-content-center'>
           <Button className='me-3' onClick={handleGoogleSignIn} variant="outline-success"><FaGoogle className='me-2'></FaGoogle>Log in With Google</Button>
           <Button onClick={handleGithubSignIN} variant="outline-secondary"><FaGithub className='me-2'></FaGithub>Log in with Github</Button>
